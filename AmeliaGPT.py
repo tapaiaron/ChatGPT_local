36b8762cc324c35d91bb6610e40b305cd92ea96b
tapaiaron <aron.tapai@gmail.com>
2023-04-16 19:06:18 +0200

    Fixing bugs, adding a more complex model.

--- AmeliaGPT.py
+++ AmeliaGPT.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 def install(package):
     try:
@@ -7,21 +8,40 @@ def install(package):
         subprocess.check_call(["pip", "install", package])
 
 # Installing language model.
-install("transformers torch")
+install("tensorflow")
+install("transformers")
 
 # Importing the downloaded modules.
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-
 def main():
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-    while True:
-        user_input = input("You: ")
-        input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
-        chat_response = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-        print("ChatGPT: " + tokenizer.decode(chat_response[:, input_ids.shape[-1]:][0], skip_special_tokens=True))
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
+    
+    def generate_response(input_text, model, tokenizer):
+        # Tokenize the input text
+        input_ids = tokenizer.encode(input_text + tokenizer.eos_token, return_tensors='pt')
 
+        # Generate a response from the model
+        response = model.generate(input_ids=input_ids, max_length=1000, do_sample=True)
+
+        # Decode the response and return it
+        return tokenizer.decode(response[0], skip_special_tokens=True)
+    
+    while (True):
+        
+        # Get user input
+        user_input = input("You: ")
+
+        # Generate a response from the ChatGPT model
+        response = generate_response(user_input, model, tokenizer)
+
+        # Print the response
+        print("AmeliaGPT: ", response)
+        
+        if "exit" in user_input.lower():
+            sys.exit()
+        
 if __name__ == "__main__":
     main()
 
